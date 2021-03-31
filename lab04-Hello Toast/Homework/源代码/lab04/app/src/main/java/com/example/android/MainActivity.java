875ff8d3ba0b85604cf1ee1eaff44601a1c73855
QinYinChang <1245776006@qq.com>
2021-03-31 17:53:45 +0800

    update

---
+++ lab04-Hello Toast/Homework/源代码/lab04/app/src/main/java/com/example/android/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private int mCount = 0;
+    private TextView mShowCount;
+    public static final String EXTRA_MESSAGE = "com.example.android.extra.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.show_count);
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this,SecondActivity.class);
+        String message = String.valueOf(mCount);
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+    }
+}
---
+++ lab04-Hello Toast/Homework/源代码/lab04/app/src/main/java/com/example/android/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    private TextView m_number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        m_number = findViewById(R.id.text_number);
+        m_number.setText(message);
+    }
+}
